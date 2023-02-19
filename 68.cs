2e9545d1eeb4becb38a187c97a9df8105be19101
mineees <minnullin09@gmail.com>
2023-02-19 22:18:15 +0300

    задача 68

---
+++ 68.cs
@@ -0,0 +1,15 @@
+﻿int AkkermanFunc(int m, int n)
+  {
+   if(m == 0)
+   {
+    return n + 1;
+   }
+   if(m > 0 && n == 0)
+   {
+    return AkkermanFunc(m - 1, 1);
+   }
+   return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+  }
+  
+  Console.WriteLine(AkkermanFunc(3,2));
+ 
