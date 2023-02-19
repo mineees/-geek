20792d40e3147a783f28bf84f85daaceb4cabd16
mineees <minnullin09@gmail.com>
2023-02-19 22:14:56 +0300

    64 задача

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintInterval(int m, int n)
+  {
+   int start = m;
+   int end = n;
+   if(m > n)
+   {
+    start = n;
+    end = m;
+   }
+   for(int i = start; i <= end; i++)
+   {
+    Console.Write($"{i} ");
+   }
+  }
+  PrintInterval(22, 9);
+  
+  Console.WriteLine();
