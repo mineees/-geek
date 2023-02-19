938e1fc0f226a1ec2101670c04a9102205efa043
mineees <minnullin09@gmail.com>
2023-02-19 22:21:02 +0300

    задaча 66

---
+++ 66.cs
@@ -0,0 +1,14 @@
+﻿int CalculateSumm(int m, int n)
+  {
+   int start = m;
+   int end = n;
+   if(m > n)
+   {
+    start = n;
+    end = m;
+   }
+   return (end + start)*(end - start + 1)/2; // по формуле суммы членов арифметической прогрессии
+  }
+  
+  Console.WriteLine(CalculateSumm(1, 15));
+ 
